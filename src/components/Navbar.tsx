f1bd2cae5d1ae2d749544c9e457ee568e85927f6
Yunsik Ohm <yunsikohm@gmail.com>
2025-02-24 11:57:17 -0800

    changed folder hierarchical structure and modified navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,21 @@
 import DarkModeToggle from "./DarkModeToggle";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav className="p-4 flex justify-between items-center bg-gray-100 dark:bg-gray-900">
-      <h1 className="text-xl font-bold text-gray-900 dark:text-white">
+    <nav className="flex justify-between items-center w-full px-6 py-4 fixed top-0 bg-white/70 dark:bg-black/70 backdrop-blur-md">
+      <Link to="/" className="text-2xl font-bold text-gray-900 dark:text-white">
         My Portfolio
-      </h1>
-      <DarkModeToggle />
+      </Link>
+      <div className="flex space-x-4">
+        <Link
+          to="/blog"
+          className="text-gray-700 dark:text-gray-300 hover:underline"
+        >
+          Blog
+        </Link>
+        <DarkModeToggle />
+      </div>
     </nav>
   );
 };
