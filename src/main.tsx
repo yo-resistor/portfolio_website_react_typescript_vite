fb56322064b3d8002cb71cf57429f3b9c8f81324
Yunsik Ohm <yunsikohm@gmail.com>
2025-02-25 09:50:19 -0800

    started side panels section

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import App from "./App.tsx";
 import Blog from "./components/Blog.tsx";
 import Navbar from "./components/Navbar.tsx";
 import Hero from "./components/Hero.tsx";
+import Side from "./components/Side.tsx";
 
 // Apply dark mode based on localStorage
 if (localStorage.getItem("theme") === "dark") {
@@ -23,6 +24,7 @@ createRoot(document.getElementById("root")!).render(
         <Route path="/" element={<App />} />
         <Route path="/blog" element={<Blog />} />
       </Routes>
+      <Side />
     </BrowserRouter>
   </StrictMode>
 );
