9a86e63d43a14794a11c027c2f83659baa12634d
Yunsik Ohm <yunsikohm@gmail.com>
2025-02-25 13:35:21 -0800

    updated the social media side bar with todo items

--- src/components/SocialSide.tsx
+++ src/components/SocialSide.tsx
@@ -2,6 +2,8 @@ import githubDark from "../assets/github-dark.svg";
 import githubLight from "../assets/github-light.svg";
 import linkedinDark from "../assets/linkedin-dark.svg";
 import linkedinLight from "../assets/linkedin-light.svg";
+import twitterDark from "../assets/twitter-dark.svg";
+import twitterLight from "../assets/twitter-light.svg";
 
 const SocialSide = () => {
   return (
@@ -47,6 +49,23 @@ const SocialSide = () => {
         {/* Google scholar icon dark and light mode */}
         {/* Instagram icon dark and light mode */}
         {/* Twitter icon dark and light mode */}
+        <a
+          href="https://www.linkedin.com/in/yohm/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="hover:scale-110 transition"
+        >
+          <img
+            src={linkedinDark}
+            alt="LinkedIn"
+            className="hidden dark:block w-6 h-6"
+          />
+          <img
+            src={linkedinLight}
+            alt="LinkedIn"
+            className="block dark:hidden w-6 h-6"
+          />
+        </a>
       </div>
       {/* Virtical line */}
       <div className="w-0.5 h-24 bg-gray-600 dark:bg-gray-300 text-sm font-medium tracking-widest hover:text-blue-500 transition rounded-md"></div>
@@ -55,3 +74,9 @@ const SocialSide = () => {
 };
 
 export default SocialSide;
+
+// TODO: google scholar dark mode svg
+// TODO: instagram light mode svg
+// TODO: instagram dark mode svg
+// TODO: Maybe I need to learn how to create he dark mode svg based on the light mode
+// TODO: how the browser makes icon dark/light: https://stackoverflow.com/questions/67187091/creating-svg-that-appears-black-in-light-mode-and-light-in-dark-mode
