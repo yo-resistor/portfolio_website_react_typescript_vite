0978719f130056b324b42f6f7fb9078adf30a5b0
Yunsik Ohm <yunsikohm@gmail.com>
2025-02-25 10:44:18 -0800

    emptied the footer and moved the code to side.tsx

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <div className="rotate-90 side__StyledSideElement-sc-1duznzb-0 jSIwrL">
+      <div className="font-mono email__StyledLinkWrapper-sc-2epoq-0 jJFfEJ fade-enter-done">
+        <a href="mailto:yunsikohm@gmail.com">yunsikohm@gmail.com</a>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
